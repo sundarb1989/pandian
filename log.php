6bcdacc35259d1ec0dba8cc665bd8acc0dd2aa92
sundar <sundar.b1989@gmail.com>
2017-06-11 02:57:16 +0530

    new commit

--- log.php
+++ log.php
@@ -71,6 +71,25 @@ echo "git test";
 		}
 		
 	}
+if(mysql_num_rows($result) == 1) 
+	{
+		while($row = mysql_fetch_assoc($result)) 
+		{
+				$_SESSION['emp_id'] = $row['emp_id'];
+				$_SESSION['login_id'] = $row['emp_code'];
+				$_SESSION['role_id'] = $row['role_id'];
+			
+		}
+		if(isset($_SESSION['login_id']))
+		{
+		 header("location: pages/view_employee.php");
+		}
+		else
+		{
+		 header("location: index.php?log=err");
+		}
+		
+	}
 	else {
 		$_SESSION['ERRMSG'] = "Invalid";
 		session_write_close();
